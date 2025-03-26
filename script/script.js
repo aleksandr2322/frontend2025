7302cdd144ff9c63213b435add2a4a89de4513ec
aleksandr2322 <pavlovskiy28201@gmail.com>
2025-03-26 16:34:16 +0000

    добавил проект

---
+++ script/script.js
@@ -0,0 +1,16 @@
+const cards={
+    card_1: {
+        name: 'Professional Profile',
+        text: 'We know finding the right job is stressful, so we’ve made it simple. It only takes a few minutes. Create a free portfolio on briefolio to show your best self and get discovered by recruiter',
+    },
+    card_2:{
+        name: ' Best Portfolio',
+        text: ' We know finding the right job is stressful, so we’ve made it simple. It only takes a few minutes. Create a free portfolio on briefolio to show your best self and get discovered by recruiter',
+    },
+    card_3:{
+        name: ' Powerful Resume',
+        text: ' We know finding the right job is stressful, so we’ve made it simple. It only takes a few minutes. Create a free portfolio on briefolio to show your best self and get discovered by',
+    },
+
+}
+
