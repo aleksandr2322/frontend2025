0a5152797b8ee7a6481774d4c58582005115b9dc
aleksandr2322 <pavlovskiy28201@gmail.com>
2025-05-07 15:49:36 +0000

    сделал модальное окно

--- script/script.js
+++ script/script.js
@@ -1,12 +1,40 @@
+ 
+//document.addEventListener('DOMContentLoaded', function() {
+   // const loginBtn = document.getElementById('Login');
+   // const registerBtn = document.getElementById('SignUp');
+   // const loginModal = document.getElementById('loginModal');
+   // const registerModal = document.getElementById('registerModal');
+   // const closeLoginModal = loginModal.querySelector('.close');
+   // const closeRegisterModal = registerModal.querySelector('.close');
+   // const slider = document.querySelector('.slider');
+    //const slides = document.querySelectorAll('.slide');
+    //const prevButton = document.querySelector('.prev-slide');
+    //const nextButton = document.querySelector('.next-slide');
+    //let currentSlide = 0;
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.slide');
+    const prevButton = document.querySelector('.prev-slide');
+    const nextButton = document.querySelector('.next-slide');
+    let currentSlide = 0;
+
+ //const modal =document.getElementById('loginmodal')
+// const btn =document.getElementById('loginBtn' )
+ //const span =document.getElementById( 'close')
+// const form =document.getElementById( 'loginform')
+ //const errorMsg = document.createElement('div');
+   // errorMsg.className = 'error-message';
+    // form.appendChild(errorMsg);
+  
+
 const cards={
-    card_1: {
-        name: 'Professional Profile',
-        text: 'We know finding the right job is stressful, so we’ve made it simple. It only takes a few minutes. Create a free portfolio on briefolio to show your best self and get discovered by recruiter',
-    },
+   card_1: {
+       name: 'Professional Profile',
+       text: 'We know finding the right job is stressful, so we’ve made it simple. It only takes a few minutes. Create a free portfolio on briefolio to show your best self and get discovered by recruiter',
+   },
     card_2:{
-        name: ' Best Portfolio',
+       name: ' Best Portfolio',
         text: ' We know finding the right job is stressful, so we’ve made it simple. It only takes a few minutes. Create a free portfolio on briefolio to show your best self and get discovered by recruiter',
-    },
+   },
     card_3:{
         name: ' Powerful Resume',
         text: ' We know finding the right job is stressful, so we’ve made it simple. It only takes a few minutes. Create a free portfolio on briefolio to show your best self and get discovered by',
@@ -14,3 +42,32 @@ const cards={
 
 }
 
+const template = `<div class="feature">
+                    <hr>
+                    <h2>${this.title}</h2>
+                    <p>${this.body}</p>
+                </div>`;
+
+const cardsContainer = document.getElementById('cards'); //ссылка для карточек 
+
+
+const modalContainer = document.querySelector('#loginModal');
+const openModalBtn = document.querySelector('#Login');
+const closeModalBtn=document.querySelector('.close')
+console.log('awwfawfawfwaf');
+
+openModalBtn.addEventListener('click', () => {
+    modalContainer.style.display = 'block';
+})
+
+closeModalBtn.addEventListener('click', () => {
+   modalContainer.style.display = 'none';
+})
+
+
+
+
+window.addEventListener('load', function() {
+    var preloader = this.document.getElementById('preloader');
+    preloader.style.display = 'none';
+});
